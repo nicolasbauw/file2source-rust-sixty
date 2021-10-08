82186475cc9c1cc530b310c76e5f7b626acaedd5
Nicolas BAUW <nbauw@hotmail.com>
2021-10-08 16:59:52 +0200

    Quit button working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::process;
 sixtyfps::include_modules!();
 
 fn main() {
@@ -9,6 +10,10 @@ fn main() {
 
     ui.on_output_added(move |text| {
         println!("Output : {}",text.to_string());
+    });
+
+    ui.on_quit_clicked( || {
+        process::exit(0);
     });
 
     ui.run();
